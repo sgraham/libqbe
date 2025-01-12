9ad771e640c017a60cb4305eab22ed012d4d93b9
Scott Graham <scott.github@h4ck3r.net>
2025-01-12 09:22:49 -0800

    wip

--- libqbe.h
+++ libqbe.h
@@ -3,111 +3,109 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef struct QQLinkage { uint32_t u; } QQLinkage;
-typedef struct QQFunc { uint32_t u; } QQFunc;
-typedef struct QQData { uint32_t u; } QQData;
-typedef struct QQType { uint32_t u; } QQType;
-typedef struct QQBlock { uint32_t u; } QQBlock;
-typedef struct QQPhi { uint32_t u; } QQPhi;
-typedef struct QQInstr { uint32_t u; } QQInstr;
-typedef struct QQJump { uint32_t u; } QQJump;
-typedef struct QQRef { uint32_t u; } QQRef;
+typedef struct QbeLinkage { uint32_t u; } QbeLinkage;
+typedef struct QbeFunc { uint32_t u; } QbeFunc;
+typedef struct QbeData { uint32_t u; } QbeData;
+typedef struct QbeType { uint32_t u; } QbeType;
+typedef struct QbeBlock { uint32_t u; } QbeBlock;
+typedef struct QbePhi { uint32_t u; } QbePhi;
+typedef struct QbeInstr { uint32_t u; } QbeInstr;
+typedef struct QbeJump { uint32_t u; } QbeJump;
+typedef struct QbeRef { uint32_t u; } QbeRef;
 
-QQType qq_builtin_type_byte();
-QQType qq_builtin_type_half();
-QQType qq_builtin_type_word();
-QQType qq_builtin_type_long();
-QQType qq_builtin_type_float();
-QQType qq_builtin_type_double();
+QbeType qbe_builtin_type_byte();
+QbeType qbe_builtin_type_half();
+QbeType qbe_builtin_type_word();
+QbeType qbe_builtin_type_long();
+QbeType qbe_builtin_type_float();
+QbeType qbe_builtin_type_double();
 
-QQData qq_begin_data(const char* name, QQLinkage linkage);
-void qq_data_byte(QQData data, uint8_t val);
-void qq_data_half(QQData data, uint16_t val);
-void qq_data_word(QQData data, uint32_t val);
-void qq_data_long(QQData data, uint64_t val);
-void qq_data_zero(QQData data, int count);
+QbeData qbe_begin_data(const char* name, QbeLinkage linkage);
+void qbe_data_byte(QbeData data, uint8_t val);
+void qbe_data_half(QbeData data, uint16_t val);
+void qbe_data_word(QbeData data, uint32_t val);
+void qbe_data_long(QbeData data, uint64_t val);
+void qbe_data_zero(QbeData data, int count);
 // TODO: string/float/double helpers
-void qq_end_data(QQData data);
+void qbe_end_data(QbeData data);
 
-QQFunc qq_begin_func(const char* name, QQLinkage linkage, QQType return_type);
+QbeFunc qbe_begin_func(const char* name, QbeLinkage linkage, QbeType return_type);
 
-void qq_func_begin_params(QQFunc func);
-void qq_func_begin_params_env(QQFunc func);
-void qq_func_param_add(QQType type, const char* name);
-void qq_func_end_params(QQFunc func);
-void qq_func_end_params_variadic(QQFunc func);
+void qbe_func_begin_params(QbeFunc func);
+void qbe_func_begin_params_env(QbeFunc func);
+void qbe_func_param_add(QbeType type, const char* name);
+void qbe_func_end_params(QbeFunc func);
+void qbe_func_end_params_variadic(QbeFunc func);
 
-QQBlock qq_begin_block(QQFunc func);
-void qq_block_add_phi(QQPhi phi);
-void qq_block_add_instr(QQInstr instr);
-void qq_block_add_jump(QQJump inst);
-void qq_end_block(QQBlock block);
+QbeBlock qbe_begin_block(QbeFunc func);
+void qbe_block_add_phi(QbePhi phi);
+void qbe_block_add_instr(QbeInstr instr);
+void qbe_block_add_jump(QbeJump inst);
+void qbe_end_block(QbeBlock block);
 
-void qq_end_func(QQFunc func);
+void qbe_end_func(QbeFunc func);
 
 // TODO: phi
 
-void qq_instr_store_d(QQBlock block, QQRef val, QQRef into);
-void qq_instr_store_s(QQBlock block, QQRef val, QQRef into);
-void qq_instr_store_l(QQBlock block, QQRef val, QQRef into);
-void qq_instr_store_w(QQBlock block, QQRef val, QQRef into);
-void qq_instr_store_h(QQBlock block, QQRef val, QQRef into);
-void qq_instr_store_b(QQBlock block, QQRef val, QQRef into);
+void qbe_instr_store_d(QbeBlock block, QbeRef val, QbeRef into);
+void qbe_instr_store_s(QbeBlock block, QbeRef val, QbeRef into);
+void qbe_instr_store_l(QbeBlock block, QbeRef val, QbeRef into);
+void qbe_instr_store_w(QbeBlock block, QbeRef val, QbeRef into);
+void qbe_instr_store_h(QbeBlock block, QbeRef val, QbeRef into);
+void qbe_instr_store_b(QbeBlock block, QbeRef val, QbeRef into);
 
-QQRef qq_instr_load_d(QQBlock block, QQRef from);
-QQRef qq_instr_load_s(QQBlock block, QQRef from);
-QQRef qq_instr_load_l(QQBlock block, QQRef from);
-QQRef qq_instr_load_sw(QQBlock block, QQRef from);
-QQRef qq_instr_load_uw(QQBlock block, QQRef from);
-QQRef qq_instr_load_sh(QQBlock block, QQRef from);
-QQRef qq_instr_load_uh(QQBlock block, QQRef from);
-QQRef qq_instr_load_sb(QQBlock block, QQRef from);
-QQRef qq_instr_load_ub(QQBlock block, QQRef from);
+QbeRef qbe_instr_load_d(QbeBlock block, QbeRef from);
+QbeRef qbe_instr_load_s(QbeBlock block, QbeRef from);
+QbeRef qbe_instr_load_l(QbeBlock block, QbeRef from);
+QbeRef qbe_instr_load_sw(QbeBlock block, QbeRef from);
+QbeRef qbe_instr_load_uw(QbeBlock block, QbeRef from);
+QbeRef qbe_instr_load_sh(QbeBlock block, QbeRef from);
+QbeRef qbe_instr_load_uh(QbeBlock block, QbeRef from);
+QbeRef qbe_instr_load_sb(QbeBlock block, QbeRef from);
+QbeRef qbe_instr_load_ub(QbeBlock block, QbeRef from);
 
 // TODO: blit
 
-QQRef qq_instr_alloc_align4(QQRef byte_count);
-QQRef qq_instr_alloc_align8(QQRef byte_count);
-QQRef qq_instr_alloc_align16(QQRef byte_count);
+QbeRef qbe_instr_alloc_align4(QbeRef byte_count);
+QbeRef qbe_instr_alloc_align8(QbeRef byte_count);
+QbeRef qbe_instr_alloc_align16(QbeRef byte_count);
 
-typedef enum QQIntCmp {
-  QQIC_INVALID,
-  QQIC_EQ,
-  QQIC_NE,
-  QQIC_SLE,
-  QQIC_SLT,
-  QQIC_SGE,
-  QQIC_SGT,
-  QQIC_ULE,
-  QQIC_ULT,
-  QQIC_UGE,
-  QQIC_UGT,
-  QQIC_COUNT
-} QQIntCmp;
+typedef enum QbeIntCmp {
+  QBE_IC_INVALID,
+  QBE_IC_EQ,
+  QBE_IC_NE,
+  QBE_IC_SLE,
+  QBE_IC_SLT,
+  QBE_IC_SGE,
+  QBE_IC_SGT,
+  QBE_IC_ULE,
+  QBE_IC_ULT,
+  QBE_IC_UGE,
+  QBE_IC_UGT,
+  QBE_IC_COUNT
+} QbeIntCmp;
 
-typedef enum QQFloatCmp {
-  QQFC_INVALID,
-  QQIC_EQ,
-  QQFC_NE,
-  QQFC_LE,
-  QQFC_LT
-  QQFC_GE,
-  QQFC_GT,
-  QQFC_O,
-  QQFC_UO,
-  QQFC_COUNT
-} QQFloatCmp;
+typedef enum QbeFloatCmp {
+  QBE_FC_INVALID,
+  QBE_FC_EQ,
+  QBE_FC_NE,
+  QBE_FC_LE,
+  QBE_FC_LT,
+  QBE_FC_GE,
+  QBE_FC_GT,
+  QBE_FC_O,
+  QBE_FC_UO,
+  QBE_FC_COUNT
+} QbeFloatCmp;
 
-QQRef qq_instr_compare_int(QQIntCmp cmp, QQRef lhs, QQRef rhs)
-QQRef qq_instr_compare_float(QQFloatCmp cmp, QQRef lhs, QQRef rhs)
-
-
-QQType qq_begin_type_struct(const char* name, int align);
-QQType qq_begin_type_union(const char* name, int align);
-void qq_type_add_type(QQType type, QQType field);
-void qq_end_type(QQType type);
-
-QQType qq_make_type_opaque(const char* name, int align, int size);
+QbeRef qbe_instr_compare_int(QbeIntCmp cmp, QbeRef lhs, QbeRef rhs);
+QbeRef qbe_instr_compare_float(QbeFloatCmp cmp, QbeRef lhs, QbeRef rhs);
 
 
+QbeType qbe_begin_type_struct(const char* name, int align /*=0*/);
+QbeType qbe_begin_type_union(const char* name, int align);
+void qbe_type_add_field(QbeType type, QbeType field);
+void qbe_type_add_field_with_count(QbeType type, QbeType field, uint32_t count);
+void qbe_end_type(QbeType type);
 
+QbeType qbe_make_type_opaque(const char* name, int align, int size);
--- libqbe_impl.c
+++ libqbe_impl.c
@@ -1,13 +1,83 @@
 #include "libqbe.h"
 
-typedef struct QQLinkageData {
+typedef struct QbeLinkageData {
   bool export;
   bool tls;
   bool common;
   const char* section_name;
   const char* section_flags;
-} QQLinkageData;
+} QbeLinkageData;
 
-QQData qq_begin_data(const char* name, QQLinkage linkage) {
-  return (QQData){0};
+QbeType qbe_begin_type_struct(const char* name, int align) {
+	Typ *ty;
+	int t, al;
+	uint n;
+
+  QbeType ret = {ntyp};
+  vgrow(&typ, ntyp+1);
+	ty = &typ[ntyp++];
+	ty->isdark = 0;
+	ty->isunion = 0;
+	ty->align = -1;
+	ty->size = 0;
+  strcpy(ty->name, name);
+  if (align != 0) {
+    for (al=0; align /= 2; al++)
+      ;
+    ty->align = al;
+  }
+  n = 0;
+  ty->fields = vnew(1, sizeof ty->fields[0], PHeap);
+  return ret;
+}
+
+void qbe_type_add_field_with_count(QbeType type, QbeType field, uint32_t count) {
+  Typ* ty1;
+  int n, a, al, type;
+  uint64_t sz, s;
+
+  Typ* ty = &typ[type.u];
+  n = 0;
+  sz = 0;
+  al = ty->align;
+  
+  ty1 = &typ[field.u];
+  s = ty1->size;
+  a = ty1->align;
+  
+  if (a > al)
+    al = a;
+  a =  (1 << a) - 1;
+  a = ((sz + a) & ~a) - sz;
+  if (a) {
+    if (n < NField) {
+      fld[n].type = FPad;
+      fld[n].len = a;
+      n++;
+    }
+  }
+  sz += a + count*s;
+  if (field.u > QBE_LAST_BASIC_TYPE) {
+    s = field.u;
+  }
+  for (; c>= 0 && n<NField; c--, n++) {
+    fld[n].type = type;
+    fld[n].len = s;
+  }
+}
+void qbe_type_add_field(QbeType type, QbeType field) {
+  qbe_type_add_field_with_count(type, field, 1);
+}
+
+void qbe_end_type(QbeType type) {
+  fld[n].type = FEnd;
+  a = 1 << al;
+  if (sz < ty->size) {
+    sz = ty->size;
+  }
+  ty->size = (sz + a - 1) & ~a;
+  ty->align = al;
+}
+
+QbeType qbe_make_type_opaque(const char* name, int align, int size) {
 }
